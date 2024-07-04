76528532e353541aa5fcdffe153a6882523db65a
Steve Popoola <stevepop@gmail.com>
2024-07-04 19:32:22 +0100

    Updated script configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	output: 'server',
+	site: 'http://stevepop.com',
+	port: 4321,
 	integrations: [mdx(), sitemap()],
+	adapter: node({
+		mode: "standalone"
+	})
 });
